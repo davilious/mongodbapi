f796676b26492d2a8bf870677362b879de5be8ed
Dávila <davila@bebanjo.com>
2018-12-30 16:38:39 +0100

    Add some mandatary fields in Card model

--- models/card.js
+++ models/card.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 
 const card = new mongoose.Schema({
   app_name: String,
-  title: String,
+  title: {
+    type: String,
+    required: true
+  },
   users: Array,
   tags: Array,
-  state: String
+  state: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('card', card)
